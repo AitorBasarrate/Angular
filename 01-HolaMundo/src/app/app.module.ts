a54d60644c3d78d9f436a62b5f82dbc629c855fb
AitorBasarrate <basarrate.aitor@gmail.com>
2020-07-03 14:04:09 +0200

    Main html changed with my component

--- 01-HolaMundo/src/app/app.module.ts
+++ 01-HolaMundo/src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {HeaderComponet} from './components/header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponet
   ],
   imports: [
     BrowserModule
---
+++ 01-HolaMundo/src/app/components/header/header.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+import { componentFactoryName } from '@angular/compiler';
+
+@Component({
+    selector: 'app-header',
+    template: `<h1>Header Component</h1>`
+})
+export class HeaderComponet{
+
+}
