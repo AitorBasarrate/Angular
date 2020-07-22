966cd179cc205b4e3464296a5165ca79fe04a689
AitorBasarrate <basarrate.aitor@gmail.com>
2020-07-22 15:05:45 +0200

    Añadido el navbar y creados componentes

--- 02-SPA/src/app/app.module.ts
+++ 02-SPA/src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-// import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { NavbarComponent } from './componentes/shared/navbar/navbar.component';
 import { HomeComponent } from './componentes/home/home.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { HeroesComponent } from './componentes/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // NavbarComponent,
-    HomeComponent
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
